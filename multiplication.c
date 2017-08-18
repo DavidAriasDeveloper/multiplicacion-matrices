89f156892e9016c5158713d2d93ac421c9e03bfc
David Arias <davidariasdeveloper@gmail.com>
2017-08-18 13:56:15 -0500

    Separacion de argumentos vs automatizacion de las matrices

--- multiplication.c
+++ multiplication.c
@@ -8,6 +8,11 @@
 #include "stdlib.h"
 #include "time.h"
 
+#define NRA 62                 /* number of rows in matrix A */
+#define NCA 15                 /* number of columns in matrix A */
+#define NRB 15                 /* number of rows in matrix A */
+#define NCB 7                  /* number of columns in matrix B */
+
 //Funcion para multiplicar matrices
 int multiplyMatrix(int a_rows,
                     int a_cols,
@@ -41,6 +46,15 @@ void fillMatrix(FILE *file,int rows,int columns,int matrix[rows][columns]){
   }
   return;
 }
+
+//Funcion para llenar matrices automaticamente
+void fillAutoMatrix(int rows,int columns,int matrix[rows][columns]){
+  int i,j = 0;
+  for (i=0; i<rows; i++)
+    for (j=0; j<columns; j++)
+      matrix[i][j]= i+j;
+}
+
 //Funcion para escribir matrices en un fichero
 void exportMatrix(FILE *file,int rows,int columns,int matrix[rows][columns]){
   fprintf(file, "%d\n%d\n", rows,columns);
@@ -85,58 +99,70 @@ int main(int argc, char **argv) {
     printf("Archivos:\n");
     printf("%s\n",argv[1]);
     printf("%s\n",argv[2]);
+
+    if(file1 != NULL){
+      //Obtenemos el numero de filas y columnas de ambas matrices
+      fscanf(file1, "%d\n%d\n",&a_rows,&a_columns);
+      fscanf(file2, "%d\n%d\n",&b_rows,&b_columns);
+    }else{
+      printf("Error, no se encuentra el archivo\n");
+      return 0;
+    }
   }else{
-    //Declaramos los archivos a leer
-    file1 = fopen("matrix_a.txt", "r");
-    file2 = fopen("matrix_b.txt", "r");
+    a_rows = NRA;
+    a_columns = NCA;
+    b_rows = NRB;
+    b_columns = NCB;
   }
+  printf("Multiplicacion de Matrices\n");
 
-  if(file1==NULL){//Comprobamos la lectura exitosa
-    printf("Error, no se encuentra el archivo\n");
-  }else{
-    printf("Multiplicacion de Matrices\n");
+  //Definimos las matrices
+  int a_matrix[a_rows][a_columns];
+  int b_matrix[b_rows][b_columns];
+  int product[a_rows][b_columns];//Se declara la matriz producto
 
-    //Obtenemos el numero de filas y columnas de ambas matrices
-    fscanf(file1, "%d\n%d\n",&a_rows,&a_columns);
-    fscanf(file2, "%d\n%d\n",&b_rows,&b_columns);
-
-    //Definimos las matrices
-    int a_matrix[a_rows][a_columns];
-    int b_matrix[b_rows][b_columns];
-
+  if(argc > 1){
     //Llenamos las matrices
     fillMatrix(file1,a_rows,a_columns,a_matrix);
     fillMatrix(file2,b_rows,b_columns,b_matrix);
-
-    //Imprimimos las matrices
-    printf("\nMatriz A: Filas: %d, Columnas: %d\n",a_rows, a_columns);
-    printMatrix(a_rows,a_columns,a_matrix);
-    printf("\nMatriz B: Filas: %d, Columnas: %d\n",b_rows, b_columns);
-    printMatrix(b_rows,b_columns,b_matrix);
-
-    if(a_columns == b_rows){
-      int product[a_rows][b_columns];//Se declara la matriz producto
-
-      t_begin = clock();
-      //Realizamos la multiplicacion
-      product[a_rows][b_columns] = multiplyMatrix(a_rows,a_columns,a_matrix,b_rows,b_columns,b_matrix,product);
-      t_end = clock();
-
-      printf("\nMatriz Producto: Filas: %d, Columnas: %d\n",a_rows,b_columns);
-      printMatrix(a_rows,b_columns,product);
-
-      //Datos de tiempo
-      secs = (double)(t_end - t_begin) / CLOCKS_PER_SEC;
-      printf("\nLa operacion se realizo en %.16g milisegundos\n", secs * 1000.0);
-
-      //Exportamos la matriz a un fichero de texto
-      FILE *export = fopen("matrix_product.txt","w");
-      exportMatrix(export,a_rows,b_columns,product);
-      fclose(export);
-    }else{
-      printf("\nNo es posible realizar la multiplicacion\n");
-    }
+  }else{
+    //Llenamos las matrices automaticamente
+    fillAutoMatrix(NRA,NCA,a_matrix);
+    fillAutoMatrix(NRB,NCB,b_matrix);
   }
+
+
+
+
+  //Imprimimos las matrices
+  printf("\nMatriz A: Filas: %d, Columnas: %d\n",a_rows, a_columns);
+  printMatrix(a_rows,a_columns,a_matrix);
+  printf("\nMatriz B: Filas: %d, Columnas: %d\n",b_rows, b_columns);
+  printMatrix(b_rows,b_columns,b_matrix);
+
+  if(a_columns == b_rows){
+    int product[a_rows][b_columns];//Se declara la matriz producto
+
+    t_begin = clock();
+    //Realizamos la multiplicacion
+    product[a_rows][b_columns] = multiplyMatrix(a_rows,a_columns,a_matrix,b_rows,b_columns,b_matrix,product);
+    t_end = clock();
+
+    printf("\nMatriz Producto: Filas: %d, Columnas: %d\n",a_rows,b_columns);
+    printMatrix(a_rows,b_columns,product);
+
+    //Datos de tiempo
+    secs = (double)(t_end - t_begin) / CLOCKS_PER_SEC;
+    printf("\nLa operacion se realizo en %.16g milisegundos\n", secs * 1000.0);
+
+    //Exportamos la matriz a un fichero de texto
+    FILE *export = fopen("matrix_product.txt","w");
+    exportMatrix(export,a_rows,b_columns,product);
+    fclose(export);
+  }else{
+    printf("\nNo es posible realizar la multiplicacion\n");
+  }
+
   fclose(file1);
   fclose(file2);
 
